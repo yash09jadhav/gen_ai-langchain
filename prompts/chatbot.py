3f3eb21429b28a724b1b5d3c04a15d8c1ad92f5f
Yash Jadhav <113256771+yash09jadhav@users.noreply.github.com>
2025-05-03 17:23:27 +0530

    Add files via upload

--- prompts/chatbot.py
+++ prompts/chatbot.py
@@ -1,15 +1,17 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
 load_dotenv()
 model = ChatGoogleGenerativeAI(model = 'gemini-1.5-pro')
-chat_history=[]
+chat_history=[SystemMessage(content='you are a very cute')]
 while True:
     user_input = input('You: ')
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
 
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print('AI: ', result.content)
 print(chat_history)
